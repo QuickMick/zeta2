465b14f5c67e44b85de1d13a4b4eeff466cc9160
mick <figura.m@web.de>
2018-07-04 01:14:40 +0200

    matched pins to new layout

--- zeta2/pins.h
+++ zeta2/pins.h
@@ -3,23 +3,23 @@
 #define pins_h
 
 /** Motor left */
-#define PIN_ENABLE_LEFT 4
-#define PIN_MOTOR_LEFT_A 5
-#define PIN_MOTOR_LEFT_B 6
+#define PIN_ENABLE_LEFT 6
+#define PIN_MOTOR_LEFT_A 11
+#define PIN_MOTOR_LEFT_B 12
 
 /** Motor right*/
-#define PIN_ENABLE_RIGHT 3
-#define PIN_MOTOR_RIGHT_A 11
+#define PIN_ENABLE_RIGHT 6
+#define PIN_MOTOR_RIGHT_A 5
 #define PIN_MOTOR_RIGHT_B 12
 
 /** LIDAR Stepper Motor */
-#define PIN_STEPPER_0 14 //A0 // (D14)
-#define PIN_STEPPER_1 15 //A1 // (D15)
-#define PIN_STEPPER_2 16 //A2 // (D16)
-#define PIN_STEPPER_3 17 //A3 // (D17)
+#define PIN_STEPPER_CURRENT_I1 A0 // D14
+#define PIN_STEPPER_CURRENT_I2 A1 // D15
+#define PIN_STEPPER_DIR A2        // D16
+#define PIN_STEPPER_STEP A6       // D20
 
 /** Fan */
-#define PIN_FAN A6 // (D20)
+#define PIN_FAN A7 // D21
 
 /** Zero Position Sensor */
 #define PIN_ZEROING_SENSOR 13
@@ -41,5 +41,8 @@
 /** Port for I2C Communication to peripherals, e.g. GY-88 - BUS 0 */
 #define I2C_PERIPHERY_SCL A5 // (D19)
 #define I2c_PERIPHERY_SDA A4 // (D18)
+
+#define SERIAL_PI_RX 9
+#define SERIAL_PI_TX 10
 
 #endif
